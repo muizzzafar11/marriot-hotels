4eeb9af4003612020775d158b47649899923a98b
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-06-17 08:07:49 -0400

    added mui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import homeImg from './assets/home-image.png'
 import './App.css';
 
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" src={homeImg} alt="logo" />
+        <img src={homeImg} className="App-logo" alt="logo" />
       </header>
       <body>
-        <img className="home-image" src={homeImg} alt="logo" />
+        <img className="App-logo" src={logo} alt="logo" />
       </body>
     </div>
   );
