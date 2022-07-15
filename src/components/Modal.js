afc0961dec703b20a4c6be2edaeb2feb7bbc8ce0
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-07-15 04:35:10 -0400

    fixed modal bug

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,17 +7,20 @@ export default function BasicModal(props) {
   const { t } = useTranslation();
   return (
     <div>
-      <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target={"#exampleModal"+details.name}>
         {t('book')}
       </button>
-      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal fade" id={"exampleModal"+details.name} tabIndex="-1" aria-labelledby={"exampleModal"+details.name+"Label"} aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <img className="" style={{height: 250}} src={details.image} alt="logo" />
             <div className="modal-body text-start">
               <h5>{t(details.name)}</h5>
               <h5>${details.price}/{t('night')}</h5>
-              <p>{t(details.description)}</p>
+              {/* <p>{t(details.description)}</p> */}
+              {t(details.description).split('<br>').map((item, index) => {
+                return <p style={{margin: 5}} key={index}>{item}</p>
+              })}
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">{t('close')}</button>
